629d4d1cd5ecb66fdd14bcb2bb118a75f676e1c0
dagba <dagdk95@gmail.com>
2020-03-18 15:33:39 +0800

    Update repository

--- lib/domain/repository/task_list_repository.dart
+++ lib/domain/repository/task_list_repository.dart
@@ -9,7 +9,6 @@ abstract class TaskListRepository {
 class TaskListRepositoryImpl implements TaskListRepository {
   List<Task> tasks = [];
 
-  TaskListRepositoryImpl({this.tasks = const []});
   @override
   void addTask({String text}) {
     final separatedText = text.split(" "); // split the text to time and body
@@ -19,22 +18,22 @@ class TaskListRepositoryImpl implements TaskListRepository {
     int year = currentTime.year;
     int month = currentTime.month;
     int day = currentTime.day;
+    final time = separatedText.first;
 
-    if (separatedText.length > 1) {
+    if (separatedText.length > 1 && time?.contains(":")) {
       completeTime =
-          Jiffy("$year-$month-$day $separatedText.first", "yyyy-MM-dd hh:mm")
-              .dateTime;
-      // if complete time is earlier than current set date time to next day
+          Jiffy("$year-$month-$day $time", "yyyy-MM-dd hh:mm").dateTime;
+      // if complete time is earlier - skip
       if (completeTime.isBefore(currentTime)) {
-        completeTime = Jiffy("$year-$month-${day + 1} $separatedText.first",
-                "yyyy-MM-dd hh:mm")
-            .dateTime;
+        return;
       }
-      body = separatedText[1];
+      body = separatedText
+          .sublist(1, separatedText.length)
+          .reduce((s1, s2) => "$s1 $s2");
     } else {
       body = text;
     }
-
+// 19:54 asdasdasdasd asdas asdasdasd
     final task = Task((b) => b
       ..body = body
       ..completeTime = completeTime);
@@ -43,11 +42,12 @@ class TaskListRepositoryImpl implements TaskListRepository {
 
   @override
   Future<List<Task>> getSortedTasks() async {
-    final noTimeTasks = tasks.where((t) => t.completeTime == null);
+    final noTimeTasks = tasks.where((t) => t.completeTime == null).toList();
 
     List<Task> withTimeSortedTasks = tasks
-      ..where((t) => t.completeTime != null)
-      ..where((t) => DateTime.now().day == t.completeTime.day);
+        .where((t) => t.completeTime != null)
+        .where((t) => DateTime.now().day == t.completeTime.day)
+        .toList();
 
     withTimeSortedTasks
         .sort((t1, t2) => t1.completeTime.compareTo(t2.completeTime));
